e70bd5fed6453e4aa90ce4e3371cad4c3d3cc3b5
mikhailrezchikov <mrezchikov529@gmail.com>
2023-09-17 16:39:49 +0300

    fix

--- src/test/java/in/reqres/specs/ReqresApiSpecs.java
+++ src/test/java/in/reqres/specs/ReqresApiSpecs.java
@@ -10,7 +10,6 @@ import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class ReqresApiSpecs {
     public static RequestSpecification userRequestSpec = with()
--- src/test/java/in/reqres/tests/ReqresApiTests.java
+++ src/test/java/in/reqres/tests/ReqresApiTests.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 import static in.reqres.specs.ReqresApiSpecs.*;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReqresApiTests {
@@ -21,14 +21,7 @@ public class ReqresApiTests {
         userData.setName("morpheus");
         userData.setJob("leader");
 
-        CreateUserResponseModel createUserResponse = step("Make request", () ->
-            given(userRequestSpec)
-                .body(userData)
-                .when()
-                .post("users")
-                .then()
-                .spec(createUserResponseSpec)
-                .extract().as(CreateUserResponseModel.class));
+        CreateUserResponseModel createUserResponse = step("Make request", () -> given(userRequestSpec).body(userData).when().post("users").then().spec(createUserResponseSpec).extract().as(CreateUserResponseModel.class));
 
         step("Check response", () -> {
             assertEquals("morpheus", createUserResponse.getName());
@@ -43,14 +36,7 @@ public class ReqresApiTests {
         userData.setName("morpheus");
         userData.setJob("zion resident");
 
-        UpdateUserResponseModel updateUserResponse = step("Make request", () ->
-            given(userRequestSpec)
-                .body(userData)
-                .when()
-                .put("users/2")
-                .then()
-                .spec(updateUserResponseSpec)
-                .extract().as(UpdateUserResponseModel.class));
+        UpdateUserResponseModel updateUserResponse = step("Make request", () -> given(userRequestSpec).body(userData).when().put("users/2").then().spec(updateUserResponseSpec).extract().as(UpdateUserResponseModel.class));
 
         step("Check response", () -> {
             assertEquals("morpheus", updateUserResponse.getName());
@@ -62,29 +48,16 @@ public class ReqresApiTests {
     @Test
     void getUsersListTest() {
 
-        step("Make request", () -> given(userRequestSpec)
-                .when()
-                .get("/users?page=2")
-                .then()
-                .spec(getUsersListResponse)
-                .body("data.size()", is(6)));
+        step("Make request", () -> given(userRequestSpec).when().get("/users?page=2").then().spec(getUsersListResponse).body("data.size()", is(6)));
     }
 
     @Test
     void deleteUserTest() {
-        step("Delete user", () -> given(userRequestSpec)
-                .when()
-                .delete("users/2")
-                .then()
-                .spec(deleteUserResponse));
+        step("Delete user", () -> given(userRequestSpec).when().delete("users/2").then().spec(deleteUserResponse));
     }
 
     @Test
     void singleUserNotFoundTest() {
-        step("Make request", () -> given(userRequestSpec)
-                .when()
-                .get("users/23")
-                .then()
-                .spec(singleUserNotFoundResponse));
+        step("Make request", () -> given(userRequestSpec).when().get("users/23").then().spec(singleUserNotFoundResponse));
     }
 }
